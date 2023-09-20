7666966b3f4b71e55eb494f63a5a1fcc8696f60e
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2023-09-20 16:17:55 +0300

    Added Contact & Stuff

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ContactValidator:AbstractValidator<Contact>
+    {
+        public ContactValidator() 
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("You Must Enter a Mail Address ! ");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("You have to Write Your Subject or Issue ! ");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Please Enter Your Username ! ");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("You Must Enter At Least 3 Characters ! ");
+            RuleFor(x => x.UserName).MinimumLength(3).WithMessage("Please check your Username , it cannot contain less than 3 characters ! ");
+            RuleFor(x => x.Subject).MaximumLength(150).WithMessage("Please , Enter Maximum 150 Characters !");
+
+        }   
+    }
+}
--- MvcProjectCamp/Controllers/ContactController.cs
+++ MvcProjectCamp/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,17 @@ namespace MvcProjectCamp.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManager cm = new ContactManager(new EFContactDal());
+        ContactValidator cv = new ContactValidator();
         public ActionResult Index()
         {
-            return View();
+            var contactvalues = cm.GetList();
+            return View(contactvalues);
+        }
+        public ActionResult ContactDetails(int id)
+        {
+            var contactvalues = cm.GetById(id);
+            return View(contactvalues);
         }
     }
 }
