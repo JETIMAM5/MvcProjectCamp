b8e5768156535cf41b41037fc604fb34efa3f5ff
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2023-10-06 14:57:50 +0300

    Writer's Page was updated . New Features have been added (Messages,Listing up to Writer,Contents) . Layouts have been updated

--- MvcProjectCamp/Controllers/WriterPanelMessageController.cs
+++ MvcProjectCamp/Controllers/WriterPanelMessageController.cs
@@ -2,6 +2,8 @@
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +31,43 @@ namespace MvcProjectCamp.Controllers
         public PartialViewResult MessageListMenu()
         {
             return PartialView();
+        }
+        public ActionResult InboxDetails(int id)
+        {
+            var values = mm.GetById(id);
+            return View(values);
+        }
+        public ActionResult SendboxDetails(int id)
+        {
+            var values = mm.GetById(id);
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message message)
+        {
+
+            ValidationResult result = messageValidator.Validate(message);
+            if (result.IsValid)
+            {
+                message.SenderMail = "eml@outlook.com";
+                message.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(message);
+                return RedirectToAction("Sendbox");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+            }
+            return View();
         }
     }
 }
