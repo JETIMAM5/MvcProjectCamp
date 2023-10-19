0b31a27a798ce9416d3efbaa48529967eed48172
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2023-10-19 20:46:53 +0300

    Added an insatnce of Google Pie Chart . It is not properly added accoring to database or main architecture . But it's just for learn how to add google chart

---
+++ MvcProjectCamp/Controllers/ChartController.cs
@@ -0,0 +1,50 @@
+﻿
+using MvcProjectCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CategoryChart() 
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<Category> BlogList() 
+        {
+        List <Category> ct = new List<Category>();
+            ct.Add(new Category
+            {
+                CategoryName = "Software",
+                CategoryCount = 2
+                
+            }); ;
+            ct.Add(new Category 
+            {
+                CategoryName="Travel",
+                CategoryCount = 3
+            });
+            ct.Add(new Category
+            {
+                CategoryName = "Technology",
+                CategoryCount = 4
+            });
+            ct.Add(new Category
+            {
+                CategoryName = "Sport",
+                CategoryCount = 1
+            });
+            return ct;
+        }
+    }
+}
