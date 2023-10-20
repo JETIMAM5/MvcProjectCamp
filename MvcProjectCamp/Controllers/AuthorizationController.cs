e050e77394eebf25a3252d95ede9d00eb48572b1
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2023-10-20 18:26:46 +0300

    Authorizations for Admin

--- MvcProjectCamp/Controllers/AuthorizationController.cs
+++ MvcProjectCamp/Controllers/AuthorizationController.cs
@@ -1,4 +1,7 @@
 ﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +10,38 @@ using System.Web.Mvc;
 
 namespace MvcProjectCamp.Controllers
 {
+    [AllowAnonymous]
     public class AuthorizationController : Controller
     {
         // GET: Authorization
+        AdminManager adm = new AdminManager(new EFAdminDal());
         public ActionResult Index()
         {
+            var adminvalues = adm.GetList();
+            return View(adminvalues);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin() 
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin admin)
+        {
+            adm.AdminAdd(admin);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+            var adminvalue = adm.GetById(id);
+            return View(adminvalue);
+        }
+        [HttpPost]
+        public ActionResult EditAdmin(Admin admin)
+        {
+            adm.AdminUpdate(admin);
+            return RedirectToAction("Index");
         }
     }
 }
